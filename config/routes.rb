623866922a5f46d6d070c6f962b726cff3f26a02
Brian Buchalter <bal711@gmail.com>
2022-02-08 06:54:38 -0700

    Remove engine mount

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,3 @@ RailsTimeTravel::Engine.routes.draw do
   root to: "manage#index"
   post "/set_datetime", to: "manage#set_datetime"
 end
-
-Rails.application.routes.draw do
-  mount RailsTimeTravel::Engine, at: "/rails/time_travel", as: 'rails_time_travel'
-end
