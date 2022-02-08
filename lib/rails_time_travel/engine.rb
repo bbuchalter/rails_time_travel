8a9679ddc400ab72b78df99034a02a0439302f78
Brian Buchalter <bal711@gmail.com>
2022-02-08 06:23:21 -0700

    Wrap in after_initialize

--- lib/rails_time_travel/engine.rb
+++ lib/rails_time_travel/engine.rb
@@ -4,8 +4,8 @@ module RailsTimeTravel
   class Engine < ::Rails::Engine
     isolate_namespace RailsTimeTravel
 
-    # config.after_initialize do
+    config.after_initialize do
       ApplicationController.send :include, RailsTimeTravel::Controller
-    # end
+    end
   end
 end
